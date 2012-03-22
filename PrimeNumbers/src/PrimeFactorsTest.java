1626db6a6ec116e97ac6e3e9297ee16f007670bb
Matthew Iwema <iwemamj@rose-hulman.edu>
2012-03-22 17:31:48 -0400

    created testOne test

--- PrimeNumbers/src/PrimeFactorsTest.java
+++ PrimeNumbers/src/PrimeFactorsTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 
@@ -7,6 +9,11 @@ public class PrimeFactorsTest {
 	@Test
 	public void testJUnit(){
 		assertTrue("The compiler isn't feeling well today...",true);
+	}
+	
+	@Test
+	public void testOne(){
+		assertEquals(new ArrayList<Integer>(), PrimeNumbers.generatePrimes(1));
 	}
 
 
