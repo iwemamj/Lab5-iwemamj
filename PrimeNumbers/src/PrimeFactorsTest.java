0d1ee2ed7cc0bdf7a4255e4ab56a50f64ecda6dd
Matthew Iwema <iwemamj@rose-hulman.edu>
2012-03-22 19:22:46 -0400

    Created testEight test (TestSix passed without method modification

--- PrimeNumbers/src/PrimeFactorsTest.java
+++ PrimeNumbers/src/PrimeFactorsTest.java
@@ -36,6 +36,11 @@ public class PrimeFactorsTest {
 		assertEquals(list(2,3),PrimeNumbers.generatePrimes(6));
 	}
 	
+	@Test
+	public void testEight(){
+		assertEquals(list(2,2,2),PrimeNumbers.generatePrimes(8));
+	}
+	
 	private ArrayList<Integer> list(int...ints){
 		ArrayList<Integer> ret = new ArrayList<Integer>();
 		for(int i : ints){
