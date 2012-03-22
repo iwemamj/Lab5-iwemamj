b173595177494afbfb98a9742c9d229fb1f2e7b7
Matthew Iwema <iwemamj@rose-hulman.edu>
2012-03-22 19:34:59 -0400

    Fixed PrimeNumbers to get testNine test to pass

--- PrimeNumbers/src/PrimeNumbers.java
+++ PrimeNumbers/src/PrimeNumbers.java
@@ -1,19 +1,20 @@
 import java.util.ArrayList;
 
-
 public class PrimeNumbers {
-	public static ArrayList<Integer> generatePrimes(int n){
+	public static ArrayList<Integer> generatePrimes(int n) {
 		ArrayList<Integer> ret = new ArrayList<Integer>();
-		
-		while(n % 2 == 0){
-			ret.add(2);
-			n /= 2;
+
+		for (int canidate = 2; n > 1; canidate++) {
+			while (n % canidate == 0) {
+				ret.add(canidate);
+				n /= canidate;
+			}
 		}
-		
-		if(n > 1){
+
+		if (n > 1) {
 			ret.add(n);
 		}
-		
+
 		return ret;
 	}
 }
