20742bfe30f32c6bd22a3f5644b413aaad8023f8
Matthew Iwema <iwemamj@rose-hulman.edu>
2012-03-22 19:18:40 -0400

    Created testSix test

--- PrimeNumbers/src/PrimeFactorsTest.java
+++ PrimeNumbers/src/PrimeFactorsTest.java
@@ -31,6 +31,11 @@ public class PrimeFactorsTest {
 		assertEquals(list(2,2),PrimeNumbers.generatePrimes(4));
 	}
 	
+	@Test
+	public void testSix(){
+		assertEquals(list(2,3),PrimeNumbers.generatePrimes(6));
+	}
+	
 	private ArrayList<Integer> list(int...ints){
 		ArrayList<Integer> ret = new ArrayList<Integer>();
 		for(int i : ints){
