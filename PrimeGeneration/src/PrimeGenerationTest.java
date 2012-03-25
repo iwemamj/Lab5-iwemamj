c172b379f1c5a74d8f8bae529edd4debcf81379e
Matthew Iwema <iwemamj@rose-hulman.edu>
2012-03-25 15:03:05 -0400

    Added first test case for testing compiler

--- PrimeGeneration/src/PrimeGenerationTest.java
+++ PrimeGeneration/src/PrimeGenerationTest.java
@@ -2,9 +2,10 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
 public class PrimeGenerationTest {
-
-
+	@Test
+	public void testJUnit() {
+		assertTrue("The compiler isn't feeling well today...", true);
+	}
 
 }
