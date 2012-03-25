1fe0bab2b05897f1dfabf82e5b3af6aa0860df20
Matthew Iwema <iwemamj@rose-hulman.edu>
2012-03-25 15:07:42 -0400

    Created Testone

--- PrimeGeneration/src/PrimeGenerationTest.java
+++ PrimeGeneration/src/PrimeGenerationTest.java
@@ -1,11 +1,18 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class PrimeGenerationTest {
 	@Test
 	public void testJUnit() {
 		assertTrue("The compiler isn't feeling well today...", true);
+	}
+	
+	@Test
+	public void testOne(){
+		assertEquals(new ArrayList<Integer>(),PrimeGeneration.FindPrime(1));
 	}
 
 }
